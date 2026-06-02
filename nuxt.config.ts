26952914652de450b693640fb84a02a6f264c97e
tissueMO <ts32worker@gmail.com>
2026-06-02 21:20:30 +0900

    ビューポートの設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,14 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   app: {
     baseURL: '/',
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+        },
+      ],
+    },
   },
   runtimeConfig: {
     slackBotToken: '',
